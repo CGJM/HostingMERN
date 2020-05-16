60692c04d3d92a6701219dea4edd7c51a9476aa7
Cristian Ortiz <criuri.069811@gmail.com>
2020-05-16 12:16:54 -0500

    Se agregan archivos de docker y nombre de Ivan

--- front-end/src/Components/others/contact.js
+++ front-end/src/Components/others/contact.js
@@ -8,16 +8,18 @@ class Contact extends Component {
       return (
           <div>
               <Navegation/>
-              <div className="container col-sm-4">
+              <div className="container col-sm-4 text-center">
                   <h1> Behind Code Zone </h1></div>
-              <div className="container col-sm-3">
+              <div className="container col-sm-3 text-center">
                   <h3> Desarrolladores: </h3></div>
-              <div className="container col-sm-4">
+              <div className="container col-sm-4 text-center">
                   <h3> Cristian Uriel Ortiz Osorio </h3></div>
-              <div className="container col-sm-4">
+              <div className="container col-sm-4 text-center">
                   <h3> Mario Etzael Francisco Melo </h3></div>
-              <div className="container col-sm-4">
+              <div className="container col-sm-4 text-center">
                   <h3> Uriel Edgardo Escobar Franco</h3></div>
+              <div className="container col-sm-4 text-center">
+                  <h3> Ivan Riveros García</h3></div>
               <Foo/>
           </div>
       )
